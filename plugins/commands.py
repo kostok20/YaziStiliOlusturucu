8b4680a3b9d874f393fce31f6d5b6c7805515847
Polat <84643819+BirBeyfendi@users.noreply.github.com>
2022-01-01 02:15:00 +0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -11,19 +11,19 @@ async def start(c, m):
     owner_username = owner.username if owner.username else 'Ns_bot_updates'
 
     # start text
-    text = f"""Hey! {m.from_user.mention(style='md')},
+    text = f"""Selam! {m.from_user.mention(style='md')},
 
-💡 ** I am Stylish Font Bot**
+💡 ** Ben Yazı Stili Oluşturucu Bot**
 
-`I can help you to get stylish fonts. Just send me some text and see magic.`
+`Bana yazı tipini değiştirmek istediğin bir kelime veya cümle yazman yeterlidir.`
 
-**👲 Maintained By:** {owner.mention(style='md')}
+**👲 Sahibim:** @Bir_Beyfendi
 """
 
     # Buttons
     buttons = [
         [
-            InlineKeyboardButton('My Father 👨‍✈️', url=f"https://t.me/{owner_username}")
+            InlineKeyboardButton('Destek grubu ✴', url="https://t.me/HerTeldenSupport")
         ]
     ]
     await m.reply_text(
@@ -64,7 +64,7 @@ async def style_buttons(c, m, cb=False):
         InlineKeyboardButton('H̆̈ă̈p̆̈p̆̈y̆̈', callback_data='style+happy'),
         InlineKeyboardButton('S̑̈ȃ̈d̑̈', callback_data='style+sad'),
         ],[
-        InlineKeyboardButton('Next ➡️', callback_data="nxt")
+        InlineKeyboardButton('İleri ➡️', callback_data="nxt")
     ]]
     if not cb:
         await m.reply_text(m.text, reply_markup=InlineKeyboardMarkup(buttons), quote=True)
@@ -101,7 +101,7 @@ async def nxt(c, m):
             InlineKeyboardButton('S̶t̶r̶i̶k̶e̶', callback_data='style+strike'),
             InlineKeyboardButton('F༙r༙o༙z༙e༙n༙', callback_data='style+frozen')
             ],[
-            InlineKeyboardButton('⬅️ Back', callback_data='nxt+0')
+            InlineKeyboardButton('⬅️ Geri', callback_data='nxt+0')
         ]]
         await m.answer()
         await m.message.edit_reply_markup(InlineKeyboardMarkup(buttons))
